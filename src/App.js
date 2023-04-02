ec829b233b7adf3b7ff32b75f7b325b8e17aa8c4
Anthony Rice <arice@madisoncollege.edu>
2023-04-01 19:13:00 -0500

    Added name to file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.  Muahahahahaha.
         </p>
+        <p>Anthony Rice</p>
         <a
           className="App-link"
           href="https://reactjs.org"
